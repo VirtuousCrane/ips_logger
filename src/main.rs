a34257000d66334f68c0205a6c19d8a2ebcaff94
VirtuousCrane <ray.daroonpat.2020@gmail.com>
2024-03-21 23:05:42 +0700

    Added: Beacon Scanning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use btleplug::{api::{Central, CentralEvent, Manager as _, Peripheral, ScanFilter}, platform::{Adapter, Manager}};
+use futures::stream::StreamExt;
 use clap::Parser;
+use tokio::{self, time};
+use uuid::{uuid, Uuid};
+use std::{error::Error, time::Duration};
+
+const BLE_BEACON_UUID: Uuid = uuid!("422da7fb-7d15-425e-a65f-e0dbcc6f4c6a");
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -23,7 +30,14 @@ struct Args {
     output: String,
 }
 
-fn main() {
+struct Beacon {
+    mac_address: String,
+    rssi: i16,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+/* 
     let args = Args::parse();
     
     println!("Host: {:?}", args.host);
@@ -31,4 +45,48 @@ fn main() {
     println!("Topic: {:?}", args.topic);
     println!("Verbose: {:?}", args.verbose);
     println!("Output: {:?}", args.output)
+*/
+    let ble_manager = Manager::new().await?;
+    let ble_adapters = ble_manager.adapters().await?;
+    let ble_central = ble_adapters.into_iter().nth(0).unwrap();
+    
+    ble_central.start_scan(ScanFilter { services: vec![BLE_BEACON_UUID] }).await?;
+
+    loop {
+        time::sleep(Duration::from_secs(2)).await;
+
+        let peripherals = ble_central.peripherals().await?;
+
+        for peripheral in peripherals.iter() {
+            let mac_address = peripheral.address();
+
+            let properties_result = peripheral.properties().await;
+            let properties = match properties_result {
+                Ok(property) => property,
+                Err(_) => {
+                    println!("Failed to extract property");
+                    continue;
+                },
+            };
+
+            let rssi_option = match properties {
+                Some(property) => property.rssi,
+                None => continue,
+            };
+
+            let rssi = match rssi_option {
+                Some(r) => r,
+                None => {
+                    println!("Failed to get RSSI");
+                    continue;
+                }
+            };
+            
+            let beacon = Beacon { mac_address: mac_address.to_string(), rssi };
+            
+            println!("Discovered: {} rssi: {}", beacon.mac_address, beacon.rssi);
+        }
+    }
+    
+    Ok(())
 }
